4aa51358d1dc578a32cba1f812969a924f78603f
jalpa9200 <jalpa9200@gmail.com>
2023-01-26 23:25:49 +0530

    login

--- db.js
+++ db.js
@@ -1,2 +1,3 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/jalpaproj');
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://jalpa:jalpa1234@cluster0.87drkzx.mongodb.net/jalpaproj?retryWrites=true&w=majority');
